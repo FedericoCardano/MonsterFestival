8147ece1d8153c899c5783b37207e895d62c7b06
PSamuele <samy.pellex@gmail.com>
2023-05-11 19:25:21 +0200

    Continuato con i Requisiti e cambiato l'Icona

--- app/src/main/java/com/example/monsterfestival/SplashActivity.java
+++ app/src/main/java/com/example/monsterfestival/SplashActivity.java
@@ -2,17 +2,43 @@ package com.example.monsterfestival;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import androidx.core.content.ContextCompat;
+import androidx.core.app.ActivityCompat;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.Looper;
+
+import android.content.pm.PackageManager;
+import android.content.pm.PackageInfo;
 
 public class SplashActivity extends AppCompatActivity {
 
+    private boolean firstLaunch = true;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        new Handler().postDelayed(new Runnable() {
+
+        if (firstLaunch) {
+            firstLaunch = false;
+
+            try {
+
+                PackageInfo packageInfo = getPackageManager().getPackageInfo(getPackageName(), PackageManager.GET_PERMISSIONS);
+                String[] PermissionsList = packageInfo.requestedPermissions;
+
+                for (int i = 0; i < PermissionsList.length; i++)
+                    if (ContextCompat.checkSelfPermission(this, PermissionsList[i]) != PackageManager.PERMISSION_GRANTED)
+                        ActivityCompat.requestPermissions(this, new String[]{PermissionsList[i]}, i);
+
+            } catch (PackageManager.NameNotFoundException e) {
+                // Handle the exception (TO DO)
+            }
+        }
+
+        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
             @Override
             public void run() {
                 startActivity(new Intent(SplashActivity.this, WelcomeActivity.class));
@@ -20,4 +46,10 @@ public class SplashActivity extends AppCompatActivity {
             }
         }, 3000);
     }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
 }

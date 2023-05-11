e9a40b42cc1041bbc012d7cd252cb26a58f26ad5
Francesco Marinelli <fra.marinelli@fastwebnet.it>
2023-05-11 10:04:32 +0200

    primo commit di Fra

--- app/src/main/java/com/example/monsterfestival/SplashActivity.java
+++ app/src/main/java/com/example/monsterfestival/SplashActivity.java
@@ -16,7 +16,7 @@ public class SplashActivity extends AppCompatActivity {
             @Override
             public void run() {
                 startActivity(new Intent(SplashActivity.this, WelcomeActivity.class));
-                finish();
+                finish();// prova poi pio oip iop
             }
         }, 3000);
     }

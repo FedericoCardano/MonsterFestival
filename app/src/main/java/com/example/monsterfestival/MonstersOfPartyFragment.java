4909a3f58e90733699d2a85de0add02f8a7a9ac9
20Cesco02 <fra.marinelli@fastwebnet.it>
2023-06-17 16:48:24 +0200

    Aggiunto fragment visualizzazione mostri nel party

---
+++ app/src/main/java/com/example/monsterfestival/MonstersOfPartyFragment.java
@@ -0,0 +1,67 @@
+package com.example.monsterfestival;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MonstersOfPartyFragment extends Fragment implements OnFragmentRemoveListener {
+
+    OnFragmentVisibleListener fragmentVisibleListener;
+
+    RecyclerView recyclerView;
+    View rootView;
+
+    public static MonstersOfPartyFragment newInstance(String param1, String param2) {
+        MonstersOfPartyFragment fragment = new MonstersOfPartyFragment();
+        Bundle args = new Bundle();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        rootView = inflater.inflate(R.layout.fragment_monsters_of_party, container, false);
+
+        recyclerView = rootView.findViewById(R.id.rvMonstersOfParty);
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(getActivity(), 1);
+        recyclerView.setLayoutManager(gridLayoutManager);
+
+        return rootView;
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentVisibleListener) {
+            fragmentVisibleListener = (OnFragmentVisibleListener) context;
+        }
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        if (fragmentVisibleListener != null) {
+            fragmentVisibleListener.onFragmentVisible(getParentFragmentManager(), this, "Dettagli Party");
+        }
+    }
+
+    public void ripristinaVisibilitaElementi() {
+
+    }
+}

1dd3e1bc8d817e493f4606b11b1910b4a51adf28
FedericoCardano <federico.cardano17@gmail.com>
2023-06-18 17:51:21 +0200

    Account (cambia Email e cambia Password)

--- app/src/main/java/com/example/monsterfestival/AccountFragment.java
+++ app/src/main/java/com/example/monsterfestival/AccountFragment.java
@@ -1,20 +1,31 @@
 package com.example.monsterfestival;
 
+import static android.content.ContentValues.TAG;
+
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.FrameLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.annotations.NotNull;
+
+import java.util.Objects;
 
 
 public class AccountFragment extends Fragment {
@@ -22,6 +33,10 @@ public class AccountFragment extends Fragment {
     View rootView;
     FirebaseAuth auth;
     Button button;
+    Button changeEmail;
+    Button changePsw;
+
+    EditText editTextEmail;
     TextView textView;
     FirebaseUser user;
 
@@ -44,8 +59,13 @@ public class AccountFragment extends Fragment {
             mostraLogin();
         }
         else{
+            changeEmail= rootView.findViewById(R.id.changeEmailButton);
+            changePsw= rootView.findViewById(R.id.changePswButton);
             textView.setText(user.getEmail());
             button.setText(getResources().getString(R.string.logout));
+            editTextEmail = rootView.findViewById(R.id.new_email);
+            changeEmail.setText(getResources().getString(R.string.cambia_email));
+            changePsw.setText(getResources().getString(R.string.cambia_psw));
         }
 
         button.setOnClickListener(new View.OnClickListener() {
@@ -56,6 +76,49 @@ public class AccountFragment extends Fragment {
                 mostraLogin();
             }
         });
+
+        changeEmail.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                if(!editTextEmail.getText().toString().isEmpty()) {
+                    user.updateEmail(editTextEmail.getText().toString())
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull @NotNull Task<Void> task) {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(getActivity(), R.string.nuova_email_poup, Toast.LENGTH_SHORT).show();
+                                    textView.setText(user.getEmail());
+                                } else {
+                                    Toast.makeText(getActivity(), R.string.cambia_email_fallito, Toast.LENGTH_SHORT).show();
+                                    Toast.makeText(getActivity(), Objects.requireNonNull(task.getException()).getMessage(), Toast.LENGTH_SHORT).show();//TODO eliminare
+                                }
+                            }
+                        });
+                }
+            }
+        });
+
+        changePsw.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                auth.sendPasswordResetEmail(Objects.requireNonNull(user.getEmail()))
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(getActivity(),R.string.nuova_psw_poup, Toast.LENGTH_SHORT).show();
+                                }
+                                else
+                                {
+                                    Toast.makeText(getActivity(),R.string.cambia_psw_fallito, Toast.LENGTH_SHORT).show();
+                                    Toast.makeText(getActivity(), Objects.requireNonNull(task.getException()).getMessage(), Toast.LENGTH_SHORT).show();//TODO eliminare
+                                }
+                            }
+                        });
+            }
+        });
+
 
         return rootView;
     }

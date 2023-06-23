45f8feb92644dd8f9ee7daea223403e3f024513a
PSamuele <samy.pellex@gmail.com>
2023-06-23 10:35:29 +0200

    Finita parte grafica settings

--- app/src/main/java/com/example/monsterfestival/fragment_dir/SettingsFragment.java
+++ app/src/main/java/com/example/monsterfestival/fragment_dir/SettingsFragment.java
@@ -1,5 +1,6 @@
 package com.example.monsterfestival.fragment_dir;
 
+import android.annotation.SuppressLint;
 import android.app.Dialog;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
@@ -12,6 +13,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
 import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.monsterfestival.R;
@@ -19,43 +22,63 @@ import com.example.monsterfestival.R;
 
 public class SettingsFragment extends Fragment {
 
-    private Button ButtonNotifiche, ButtonFeedback, ButtonCredit;
+    private SeekBar SeekBarAggiornamenti;
+    private Button ButtonAggiornamenti, ButtonFeedback, ButtonCredit;
+
+    private TextView TextAggiornamenti;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_settings, container, false);
 
-        ButtonNotifiche = view.findViewById(R.id.ButtonNotifiche);
+        SeekBarAggiornamenti = view.findViewById(R.id.seekBarAggiornamenti);
+        TextAggiornamenti = view.findViewById(R.id.perdiodoAggiornamenti);
+
+        ButtonAggiornamenti = view.findViewById(R.id.ButtonAggiornamenti);
         ButtonFeedback = view.findViewById(R.id.ButtonFeedback);
         ButtonCredit = view.findViewById(R.id.ButtonCredit);
 
-        ButtonNotifiche.setOnClickListener(new View.OnClickListener() {
+        SeekBarAggiornamenti.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+
             @Override
-            public void onClick(View view) {
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @SuppressLint("SetTextI18n")
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress,boolean fromUser) {
+                if (progress == 0)
+                    TextAggiornamenti.setText("Mai");
+                else if (progress == 1)
+                    TextAggiornamenti.setText("1 Giorno");
+                else
+                    TextAggiornamenti.setText(progress + " Giorni");
+
+
 
             }
         });
 
-        ButtonFeedback.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(getActivity(), getResources().getString(R.string.funzione_in_arrivo), Toast.LENGTH_SHORT).show();
-            }
+        ButtonAggiornamenti.setOnClickListener(view14 -> {
+
         });
 
-        ButtonCredit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Dialog dialog = new Dialog(getContext());
-                dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
-                dialog.setContentView(R.layout.popup_conferma_cancellazione);
-                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
-                dialog.setCancelable(true);
-                dialog.show();
+        ButtonFeedback.setOnClickListener(view13 -> Toast.makeText(getActivity(), getResources().getString(R.string.funzione_in_arrivo), Toast.LENGTH_SHORT).show());
 
-                dialog.findViewById(R.id.btnNo).setOnClickListener(view1 -> dialog.dismiss());
-            }
+        ButtonCredit.setOnClickListener(view12 -> {
+            Dialog dialog = new Dialog(getContext());
+            dialog.requestWindowFeature(Window.FEATURE_NO_TITLE);
+            dialog.setContentView(R.layout.popup_conferma_cancellazione);
+            dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+            dialog.setCancelable(true);
+            dialog.show();
+
+            dialog.findViewById(R.id.btnNo).setOnClickListener(view1 -> dialog.dismiss());
         });
 
         return view;

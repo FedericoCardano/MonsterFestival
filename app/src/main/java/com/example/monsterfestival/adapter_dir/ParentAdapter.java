b955fd6da0a464fdfefc9ab2c7bb82b13b60d228
PSamuele <samy.pellex@gmail.com>
2023-06-24 14:12:19 +0200

    Dimenticato import inutile

--- app/src/main/java/com/example/monsterfestival/adapter_dir/ParentAdapter.java
+++ app/src/main/java/com/example/monsterfestival/adapter_dir/ParentAdapter.java
@@ -6,7 +6,6 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.ViewTreeObserver;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
